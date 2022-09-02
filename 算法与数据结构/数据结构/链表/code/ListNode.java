2955d4c2082f42721440429696ec92aeb7c62c21
BlueFish <chenlanqing@users.noreply.github.com>
2022-09-02 14:26:25 +0800

    add method in ListNode

--- 算法与数据结构/数据结构/链表/code/ListNode.java
+++ 算法与数据结构/数据结构/链表/code/ListNode.java
@@ -1,5 +1,3 @@
-package com.exe.linkedlist;
-
 public class ListNode {
 	public int val;
 	public ListNode next;
@@ -52,4 +50,33 @@ public class ListNode {
 		p.next = head.next;
 		return head.next;
 	}
+
+	/**
+     * 创建相交的两个链表
+     * @param startIndex 相交链表的起始索引
+     */
+    public static ListNode[] arrayToIntersection(int[] arr1, int[] arr2, int startIndex) {
+        ListNode head1 = new ListNode(0);
+        ListNode head2 = new ListNode(0);
+        int m = arr1.length;
+        int n = arr2.length;
+        ListNode p1 = head1;
+        ListNode p2 = head2;
+        ListNode intersection = null;
+        for (int i = 0; i < m; i++) {
+            ListNode p = new ListNode(arr1[i]);
+            p1.next = p;
+            p1 = p1.next;
+            if (i == startIndex - 1) { // i 从 0 开始计算， startIndex 从 1 开始计算
+                intersection = p1;
+            }
+        }
+        for (int j : arr2) {
+            ListNode p = new ListNode(j);
+            p2.next = p;
+            p2 = p2.next;
+        }
+        p2.next = intersection;
+        return new ListNode[]{head1.next, head2.next};
+    }
 }
