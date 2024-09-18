777ccb1d7205cf8895571061814256c89348240c
BlueFish <chenlanqing@users.noreply.github.com>
2024-09-18 19:04:40 +0800

    rsa utils

---
+++ Java/代码片段/source/RSAUtils.java
@@ -0,0 +1,67 @@
+import javax.crypto.Cipher;
+import java.security.*;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+public final class RSAUtils {
+    /**
+     * 加密算法
+     */
+    private final static String ALGORITHM = "RSA";
+
+    /**
+     * 生成密钥对
+     */
+    public static KeyPair getKeyPair() throws Exception {
+        // 创建KeyPairGenerator对象，指定算法为RSA
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance(ALGORITHM);
+        keyPairGen.initialize(2048); // 设置密钥长度
+        // 生成密钥对
+        return keyPairGen.generateKeyPair();
+    }
+
+    /**
+     * 对公钥或者私钥进行Base64编码
+     */
+    public static String base64Key(Key key) {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+    public static String encrypt(String data, String publicKeyStr) throws Exception {
+        // 解码Base64字符串
+        byte[] keyBytes = Base64.getDecoder().decode(publicKeyStr);
+        // 使用X509EncodedKeySpec来定义公钥的编码
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        // 获取KeyFactory并生成公钥
+        KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM);
+        return encrypt(data, keyFactory.generatePublic(keySpec));
+    }
+
+    public static String encrypt(String data, PublicKey publicKey) throws Exception {
+        // 创建Cipher对象，指定算法为RSA
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey); //
+        byte[] encryptedBytes = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes); // 将加密结果编
+    }
+
+
+    public static String decrypt(String data, String privateKeyStr) throws Exception {
+        // 解码Base64字符串
+        byte[] keyBytes = Base64.getDecoder().decode(privateKeyStr);
+        // 使用X509EncodedKeySpec来定义公钥的编码
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        // 获取KeyFactory并生成公钥
+        KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM);
+        return decrypt(data, keyFactory.generatePrivate(keySpec));
+    }
+
+    public static String decrypt(String data, PrivateKey privateKey) throws Exception {
+        // 创建Cipher对象，指定算法为RSA
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey); // 设置为解密模式并使用私钥
+        // 解密数据
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(data));
+        return new String(decryptedBytes);
+    }
+}
